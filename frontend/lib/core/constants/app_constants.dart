d516d33a0dcc703c94e2aa1911e146fc826fc7dc
Jakson <jaksonrichard34@gmail.com>
2026-05-16 23:22:18 +0700

    feat(frontend): add app global constants

---
+++ frontend/lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  const AppConstants._();
+
+  static const String appName = 'Food Scanner App';
+  static const String defaultUserName = 'Jakson';
+  static const int dailyCalorieTarget = 2200;
+}
